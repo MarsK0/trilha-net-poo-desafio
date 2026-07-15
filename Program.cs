ff66da14ccb913cd7d67db9f7f73f32bec38c26a
Marco <marcosilhaag31@gmail.com>
2026-07-15 18:56:38 -0300

    concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("12345678", "tijolão", "1234321", 32);
+Iphone iphone = new Iphone("87654321", "pomposo", "4321234", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("jogo da cobrinha");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("ZapZap");
